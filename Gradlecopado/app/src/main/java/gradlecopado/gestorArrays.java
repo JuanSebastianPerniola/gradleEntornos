8cb796c80a8b845e1dd1deca19593ac853f75633
Juan Sebastian Perniola <146111021+JuanSebastianPerniola@users.noreply.github.com>
2024-02-26 20:22:36 +0100

    Add files via upload

---
+++ Gradlecopado/app/src/main/java/gradlecopado/gestorArrays.java
@@ -0,0 +1,51 @@
+package gradlecopado;
+
+import org.checkerframework.checker.units.qual.A;
+import java.util.Arrays;
+
+public class gestorArrays {
+    // comptarElements(): Retorna el numero d'elements de l'array.
+
+   
+    public static int ElementsCount(int[] array) {
+        return array.length;
+    }
+
+    public static int firstElement(int[] array) {
+        
+        return array[0];
+    }
+
+    public static int lastElement(int[] array) {
+        
+        return array[7];
+    }
+
+    public static int thirdPosition(int[] array) throws IllegalArgumentException {
+        
+        if (array.length < 3) {
+            throw new IllegalArgumentException("The array is no longer than 3 numbers ");
+        } 
+        return array[2];
+    }
+
+    public static int sumaTotal(int[] arraySum) {
+        int count = 0;
+        for (int value : arraySum) {
+            count += value;
+        }
+        return count;
+    }
+    public static double mediaTotal(int[] array) {
+        if (array == null) {
+            return 0;
+        }
+    
+        double count = 0;
+        for (int value : array) {
+            count += value;
+        }
+        return count / array.length; // Division done with a double
+    }
+
+}
